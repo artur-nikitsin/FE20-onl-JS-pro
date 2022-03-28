d94da44d08555679fd453c779a89f64c86e46e19
kryshtafovich.nastya <miksturchik123@gmail.com>
2022-03-28 22:32:14 +0300

    fix: fixed changes in  js file

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 console.log("Hello world");
+<<<<<<< HEAD
 console.log("Hello world!!!");
+=======
+console.log("Hello world!");
+console.log("Hello world!!");
+>>>>>>> 861e330 (fix: fixed changes in  js file)
